299537ee4bf2f08b212da0d38c312849c13c86dc
ryan <4721319+rpshukla@users.noreply.github.com>
2018-06-17 17:53:22 -0600

    Created function init_taskfiles_directory

---
+++ src/init_taskfiles_directory.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+def init_taskfiles_directory():
+# creates taskfiles directory if none exists
+# returns Path object for taskfiles directory
+
+    taskfiles_path = Path('../taskfiles')
+    if taskfiles_path.exists():
+        print('taskfiles directory exists')
+    else:
+        print('No taskfiles directory found. Creating new one.')
+        taskfiles_path.mkdir()
